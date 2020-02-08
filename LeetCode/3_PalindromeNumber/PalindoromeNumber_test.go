fcb5abc2b83e142f1a405b82659033a3046fac79
akira uemoto <empty.aauu0001@gmail.com>
2020-02-08 19:41:09 +0900

    ディレクトリ変更

--- LeetCode/3_PalindromeNumber/PalindoromeNumber_test.go
+++ LeetCode/3_PalindromeNumber/PalindoromeNumber_test.go
@@ -1,14 +1,14 @@
-package PalindromeNumber_test
+package __PalindromeNumber_test
 
 import (
-	"github.com/a1008u/GoTraning/LeetCode/PalindromeNumber"
+	"github.com/a1008u/GoTraning/LeetCode/3_PalindromeNumber"
 	"testing"
 )
 
 func Test_IsPalindrome_OK_1(t *testing.T) {
 
 	targetNumber := 121
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if !actual {
 		t.Errorf("Error %v", targetNumber)
 	}
@@ -17,7 +17,7 @@ func Test_IsPalindrome_OK_1(t *testing.T) {
 func Test_IsPalindrome_OK_2(t *testing.T) {
 
 	targetNumber := 1221
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if !actual {
 		t.Errorf("Error %v", targetNumber)
 	}
@@ -26,7 +26,7 @@ func Test_IsPalindrome_OK_2(t *testing.T) {
 func Test_IsPalindrome_OK_3(t *testing.T) {
 
 	targetNumber := 12521
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if !actual {
 		t.Errorf("Error %v", targetNumber)
 	}
@@ -35,7 +35,7 @@ func Test_IsPalindrome_OK_3(t *testing.T) {
 func Test_IsPalindrome_NG_1(t *testing.T) {
 
 	targetNumber := -121
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if actual {
 		t.Errorf("Error %v", targetNumber)
 	}
@@ -44,7 +44,7 @@ func Test_IsPalindrome_NG_1(t *testing.T) {
 func Test_IsPalindrome_NG_2(t *testing.T) {
 
 	targetNumber := 10
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if actual {
 		t.Errorf("Error %v", targetNumber)
 	}
@@ -53,7 +53,7 @@ func Test_IsPalindrome_NG_2(t *testing.T) {
 func Test_IsPalindrome_NG_3(t *testing.T) {
 
 	targetNumber := 120
-	actual := PalindromeNumber.IsPalindrome(targetNumber)
+	actual := __PalindromeNumber.IsPalindrome(targetNumber)
 	if actual {
 		t.Errorf("Error %v", targetNumber)
 	}
